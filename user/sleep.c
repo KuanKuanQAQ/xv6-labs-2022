582bcd729a75b8859e965e822a81739a8afaccba
KuanKuanQAQ <leaker233@icloud.com>
2023-04-03 15:26:08 +0800

    sleep

--- user/sleep.c
+++ user/sleep.c
@@ -3,10 +3,10 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-  if(argc < 2){
+  if (argc < 2) {
     fprintf(2, "Usage: sleep for a new sec...\n");
     exit(-1);
   }
-  sleep(atoi(argv[1]));
+  sleep(atoi(argv[1]) * 10);
   exit(0);
 }
