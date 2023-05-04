285d7262bbe21033130bc2e0ca7bc6e53a2589e5
KuanKuanQAQ <leaker233@icloud.com>
2023-05-04 15:36:07 +0800

    Initial commit

--- kernel/vm.c
+++ kernel/vm.c
@@ -151,10 +151,8 @@ mappages(pagetable_t pagetable, uint64 va, uint64 size, uint64 pa, int perm)
   a = PGROUNDDOWN(va);
   last = PGROUNDDOWN(va + size - 1);
   for(;;){
-    if((pte = walk(pagetable, a, 1)) == 0) {
+    if((pte = walk(pagetable, a, 1)) == 0)
       return -1;
-    }
-      
     if(*pte & PTE_V)
       panic("mappages: remap");
     *pte = PA2PTE(pa) | perm | PTE_V;
